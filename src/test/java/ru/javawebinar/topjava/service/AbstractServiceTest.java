bb9ca0ddda8dc5dee1bdf877e2b7bb9289020ac5
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-03-13 12:44:25 +0100

    6_02_HW5_profile_test.patch

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -1,20 +1,21 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.ClassRule;
+import org.junit.AfterClass;
 import org.junit.Rule;
-import org.junit.rules.ExternalResource;
 import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
-import ru.javawebinar.topjava.TimingRules;
 
-import static org.junit.Assert.assertThrows;
-import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
+import java.util.concurrent.TimeUnit;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
@@ -24,21 +25,30 @@ import static ru.javawebinar.topjava.util.ValidationUtil.getRootCause;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 @ActiveProfiles(resolver = ActiveDbProfileResolver.class)
 public abstract class AbstractServiceTest {
+    private static final Logger log = getLogger("result");
 
-    @ClassRule
-    public static ExternalResource summary = TimingRules.SUMMARY;
+    private static final StringBuilder results = new StringBuilder();
 
     @Rule
-    public Stopwatch stopwatch = TimingRules.STOPWATCH;
+    // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
+    public Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void finished(long nanos, Description description) {
+            String result = String.format("%-95s %7d", description.getDisplayName(), TimeUnit.NANOSECONDS.toMillis(nanos));
+            results.append(result).append('\n');
+            log.info(result + " ms\n");
+        }
+    };
 
-    //  Check root cause in JUnit: https://github.com/junit-team/junit4/pull/778
-    protected <T extends Throwable> void validateRootCause(Class<T> rootExceptionClass, Runnable runnable) {
-        assertThrows(rootExceptionClass, () -> {
-            try {
-                runnable.run();
-            } catch (Exception e) {
-                throw getRootCause(e);
-            }
-        });
+
+    //    https://dzone.com/articles/applying-new-jdk-11-string-methods
+    private static final String DELIM = "-".repeat(103);
+
+    @AfterClass
+    public static void printResult() {
+        log.info("\n" + DELIM +
+                "\nTest                                                                                       Duration, ms" +
+                "\n" + DELIM + "\n" + results + DELIM + "\n");
+        results.setLength(0);
     }
 }
